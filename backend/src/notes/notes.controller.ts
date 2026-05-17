1b7b2105f2ca40986a57f185f7c59dd48f60b0d8
Andrew <andrewmeier.dev@gmail.com>
2026-05-17 02:21:35 -0300

    update del backend

--- backend/src/notes/notes.controller.ts
+++ backend/src/notes/notes.controller.ts
@@ -1,10 +1,25 @@
 import { NotesService } from './notes.service';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Patch,
+} from '@nestjs/common';
 import { CreateNoteDto } from './dto/create-note.dto';
+import { UpdateNoteDto } from './dto/update-note.dto';
 
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
+
+  @Get('archived')
+  findArchived() {
+    return this.notesService.findArchived();
+  }
 
   @Get()
   findAll() {
@@ -14,5 +29,20 @@ export class NotesController {
   @Post()
   create(@Body() body: CreateNoteDto) {
     return this.notesService.create(body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.notesService.remove(id);
+  }
+
+  @Patch(':id/archive')
+  archive(@Param('id', ParseIntPipe) id: number) {
+    return this.notesService.archive(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateNoteDto) {
+    return this.notesService.update(id, body);
   }
 }
--- backend/src/notes/notes.service.ts
+++ backend/src/notes/notes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateNoteDto } from './dto/create-note.dto';
+import { UpdateNoteDto } from './dto/update-note.dto';
 
 @Injectable()
 export class NotesService {
@@ -19,6 +20,45 @@ export class NotesService {
 
   async create(data: CreateNoteDto) {
     return this.prisma.note.create({
+      data,
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.note.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async archive(id: number) {
+    return this.prisma.note.update({
+      where: {
+        id,
+      },
+      data: {
+        archived: true,
+      },
+    });
+  }
+
+  async findArchived() {
+    return this.prisma.note.findMany({
+      where: {
+        archived: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  async update(id: number, data: UpdateNoteDto) {
+    return this.prisma.note.update({
+      where: {
+        id,
+      },
       data,
     });
   }
